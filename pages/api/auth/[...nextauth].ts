7a239de52dd59d0834f56d0ca40291740515ee09
chinoshuta <chinoshuta@gmail.com>
2022-11-06 13:40:34 +0900

    auth 修正

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -25,14 +25,14 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.Google_CLIENT_ID!,
       clientSecret: process.env.Google_CLIENT_SECRET!,
-      // profile(profile) {
-      //   return {
-      //     id: profile.id.toString(),
-      //     name: profile.name || profile.login,
-      //     gh_username: profile.login,
-      //     email: profile.email,
-      //   };
-      // },
+      profile(profile) {
+        return {
+          id: profile.id.toString(),
+          name: profile.name || profile.login,
+          gh_username: profile.login,
+          email: profile.email,
+        };
+      },
     }),
   ],
   pages: {
